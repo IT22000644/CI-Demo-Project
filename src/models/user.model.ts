36091556a81bdd2401bf7faaf7db68f036b6a39d
Manilka Navod <manilka.projects@gmail.com>
2025-03-17 20:17:19 +0530

    user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,16 @@
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+    createdAt: Date;
+    updatedAt: Date;
+    location: location;
+}
+
+interface location {
+    longitude: number;
+    latitude: number;
+}
+
