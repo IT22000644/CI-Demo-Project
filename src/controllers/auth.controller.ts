cfa71f38f0ca753b46c5912a83ab0c0a91887ca9
Manilka Navod <manilka.projects@gmail.com>
2025-03-27 21:53:58 +0530

    user controller setup

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,1 +1,12 @@
-class AuthController {}
+interface SignUpRequestBody {
+    email: string;
+    password: string;
+}
+
+
+class AuthController {
+
+    async signUp(req: Request, res: Response) {
+
+    }
+}
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,2 +1,11 @@
+// generate an user Schema
 
+interface User {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
 
